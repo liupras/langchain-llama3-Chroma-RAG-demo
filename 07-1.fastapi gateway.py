79aad49cbf8f5a9f81c07bc7b51a3d1675ce2987
liulj <56008507@qq.com>
2024-11-14 12:59:44 +0800

    rename

--- 07-1.fastapi gateway.py
+++ 07-1.fastapi gateway.py
@@ -47,6 +47,6 @@ if __name__ == "__main__":
     import uvicorn
 
     # 交互式API文档地址：
-    # http://127.0.0.1:5001/docs/ 
-    # http://127.0.0.1:5001/redoc/
+    # http://127.0.0.1:8000/docs/ 
+    # http://127.0.0.1:8000/redoc/
     uvicorn.run(app, host="0.0.0.0", port=8000)
